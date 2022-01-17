2197647fe799d8b1075a5ce6457e74bc5e6e295b
RND332 <eureq631@gmail.com>
2022-01-17 21:16:19 +0300

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,11 +57,6 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	!function() {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	}();
-/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -136,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4ea624544b8b2ad9"; }
+/******/ 		__webpack_require__.h = function() { return "6ae1064e529c8c59"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
